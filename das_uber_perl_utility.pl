bcb278981e14561afb778a90d3a7ce2dd6cce283
Jeremy Falling <jjfalling@gmail.com>
2013-03-03 11:01:48 -0800

    Update das_uber_perl_utility.pl

--- das_uber_perl_utility.pl
+++ das_uber_perl_utility.pl
@@ -2,6 +2,28 @@
 #This is used to interface with other programs that are not meant to be interactivly run.
 # - JFalling 2012
 
+#****************************************************************************
+#*   Das Über Perl Utility                                                  *
+#*   This is used to interface with other programs that are not meant to    *
+#*   be interactivly run.                                                   *
+#*                                                                          *
+#*   Copyright (C) 2013 by Jeremy Falling except where noted.               *
+#*                                                                          *
+#*   This program is free software: you can redistribute it and/or modify   *
+#*   it under the terms of the GNU General Public License as published by   *
+#*   the Free Software Foundation, either version 3 of the License, or      *
+#*   (at your option) any later version.                                    *
+#*                                                                          *
+#*   This program is distributed in the hope that it will be useful,        *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+#*   GNU General Public License for more details.                           *
+#*                                                                          *
+#*   You should have received a copy of the GNU General Public License      *
+#*   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+#****************************************************************************
+
+
 use strict;
 use warnings;
 use Term::ANSIColor;
